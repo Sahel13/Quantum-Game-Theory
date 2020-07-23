e8bbaedd42009c9952cc83b3eeb5527d3d5067f1
Sahel Mohammad Iqbal <sahel13miqbal@gmail.com>
2020-07-23 15:09:33 +0530

    Code formatted for readability

--- PrisonersDilemma.py
+++ PrisonersDilemma.py
@@ -36,21 +36,24 @@ class PrisonersDilemma():
 			'DD': [1,1],
 		}
 
+
 	def playerMove(self, theta, phi):
 		"""
-		Possible moves that can be taken by the players.
+		Possible moves that can be chosen by the players.
 		Returns unitary matrices that are parameterized by two variables.
 		"""
 		cosine = math.cos(theta/2)
 		sine = math.sin(theta/2)
 		matrix = np.array([[cmath.exp(1j*phi)*cosine, sine],[-sine, cmath.exp(-1j*phi)*cosine]])
+
 		return matrix
 
-	# J matrix (creates entanglement)
+
 	def setJ(self, gamma):
 		"""
+		Creates entanglement between the players.
 		Gamma is the entanglement parameter.
-		0 for no entanglement (classical case), pi/2 for maximal entanglement.
+		0 for no entanglement, pi/2 for maximal entanglement.
 		"""
 		cosine = math.cos(gamma/2)
 		sine = 1j*math.sin(gamma/2)
@@ -64,25 +67,44 @@ class PrisonersDilemma():
 
 		self.J = matrix
 	
+
 	def setAliceMove(self, theta, phi):
+		"""
+		Choose Alice's move. Takes two angles as parameters.
+		"""
 		self.U_A = self.playerMove(theta, phi)
 	
+
 	def setBobMove(self, theta, phi):
+		"""
+		Choose Bob's move. Takes two angles as parameters.
+		"""
 		self.U_B = self.playerMove(theta, phi)
 
+
 	def setOutput(self):
+		"""
+		Calculates the output given J and player moves.
+		"""
 		initialState = np.matmul(self.J, self.GameStates['CC'])
 		gameMoves = np.kron(self.U_A, self.U_B)
 		lastStep = np.matmul(Hermitian(self.J), gameMoves)
 		output = np.matmul(lastStep, initialState)
+
 		self.Output = output
 
 	def expectedPayoff(self):
+		"""
+		Returns the expected payoffs for Alice and Bob as a tuple.
+		"""
 		self.setOutput()
+
 		alice = 0
 		bob = 0
 		for key in self.GameStates:
 			amplitude = np.dot(self.GameStates[key], self.Output)
-			alice += self.PayoffMatrix[key][0] * abs(amplitude)**2
-			bob += self.PayoffMatrix[key][1] * abs(amplitude)**2
+			stateProb = abs(amplitude)**2
+			alice += self.PayoffMatrix[key][0] * stateProb
+			bob += self.PayoffMatrix[key][1] * stateProb
+
 		return (alice, bob)
